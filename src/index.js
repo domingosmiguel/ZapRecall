58282d082c86c8a3358607e4b0f2f41a5da7d43c
domingosmiguel <migueldsp@hotmail.com>
2022-09-29 19:08:31 -0300

    warnings fix 2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-import ReactDOM from "react-dom";
+// OLD WAY
+// import ReactDOM from "react-dom";
+// import App from "./App";
+
+// ReactDOM.render(<App />, document.querySelector("#root"));
+
+// NEW WAY WAY NODE.JS 18+
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+createRoot(document.querySelector("#root")).render(<App />);
+
+// step-by-step ----->
+
+// const container = document.querySelector("#root");
+// const root = createRoot(container);
+
+// root.render(<App />);
